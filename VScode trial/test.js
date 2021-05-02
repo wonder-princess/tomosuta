b83b42d816ae37cf42464feadfc785fa1b2c5187
wonder_princess <sekai_love.me.do@hotmail.co.jp>
2021-05-02 17:06:05 +0900

    VScode trial

---
+++ VScode trial/test.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+res.context
+context.strokeStyle = 'blue';
+context.lineWidth
+document.body
+
+// Reference the function
+new books("War of the Worlds", "H G Wells");
+new books("The Martian", "Andy Weir");
+
+/**
+ * Represents a book.
+ *
+ * @param {string} title Title of the book
+ * @param {string} author Who wrote the book
+ */
+function books(title, author) {
+    this.title = title;
+    this.author = author;
+}
+
+const cars = ["🚗", "🚙", "🚕"];
+
+for (const car of cars){
+    // Drive the car
+        console.log(`This is the car ${car}`);
+}
+
---
+++ VScode trial/test.ts
@@ -0,0 +1,21 @@
+// This code has a few syntax errors
+console.log(add(1, 1.5));
+
+
+function Add(a : Number, b : Number) : Int {
+    return a + b;
+}
+try {
+    
+} catch (error) {
+    
+}
+if (condition) {
+    
+} else {
+    
+}
+// @ts-nocheck
+
+let easy = true;
+easy = 42;
