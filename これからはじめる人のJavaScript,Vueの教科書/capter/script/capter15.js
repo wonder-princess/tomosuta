882f7bdbdfa1f9672def21c02e6bc4e01d6f3f0a
wonder_princess <sekai_love.me.do@hotmail.co.jp>
2021-08-02 19:38:52 +0900

    cap15

--- これからはじめる人のJavaScript,Vueの教科書/capter/script/capter15.js
+++ これからはじめる人のJavaScript,Vueの教科書/capter/script/capter15.js
@@ -1,31 +1,46 @@
-
-const tax = new Tax;
-tax.setRate = 10;
-
-const TAX = tax.salesTax;
-
 // 標準値を設定
 const getTax = (price, tax=10) => {
     return Math.round(price * (tax / 100));
 }
+console.log(getTax(1980));
 
-//console.log(getTax(1980));
+/***** 税の値をカプセル化 *****/
 
 class Tax {
     value = 100;
-    rate;
-    constructor () {
-        const salesTax = (this.rate / this.value)
-    }
-    static setRate(val){
+    salesTax;
+    set salesTaxRate(val){
         try {
-            this.rate = val;
+            this.salesTax = (val / this.value);
         } catch (error) {
             console.log(error.message);
         }
     }
 }
 
+/******************************/
+
+
+/********** 税率を設定 **********/
+
+const tax = new Tax;
+tax.salesTaxRate = 10;
+
+/******************************/
+
+
+/***** 設定した値を取得・計算 *****/
+
+const SALES_TAX = tax.salesTax;
+console.log(SALES_TAX);
+
+function taxCalc(price) {
+    return Math.round(price * SALES_TAX);
+}
+
+console.log(taxCalc(8888));
+
+/******************************/
 
 
 
